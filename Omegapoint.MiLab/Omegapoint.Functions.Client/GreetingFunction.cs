279b06bd16f4037be284f1556053967c2b5972d5
opadalin <adam.lindstrom@omegapoint.se>
2023-02-03 17:27:20 +0100

    add server and client functions

---
+++ Omegapoint.MiLab/Omegapoint.Functions.Client/GreetingFunction.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Omegapoint.Functions.Client;
+
+public class GreetingFunction
+{
+    private readonly HttpClient _httpClient;
+
+    public GreetingFunction(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+        _httpClient.BaseAddress = new Uri("https://func-lindis.azurewebsites.net/");
+    }
+
+    [FunctionName("GreetingFunction")]
+    public async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "greeting")]
+        HttpRequest req, ILogger log)
+    {
+        log.LogInformation("Greeting is sent to server");
+
+        var scopes = new[]
+        {
+            ""
+        };
+
+        var managedIdentityAzureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions());
+        var accessToken = await managedIdentityAzureCredential.GetTokenAsync(new TokenRequestContext(scopes));
+        var token = accessToken.Token;
+        
+        var httpRequest = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("hello"),
+            Headers = {{"Authorization", token}}
+        };
+
+        var response = await _httpClient.SendAsync(httpRequest);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return new ObjectResult(HttpStatusCode.InternalServerError +
+                                    ": Received unsuccessful response from server");
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        return await Task.FromResult(new OkObjectResult($"Response from server: {content}"));
+    }
+}
---
+++ Omegapoint.MiLab/Omegapoint.Functions.Server/HelloServerFunction.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Omegapoint.Functions.Server;
+
+public static class HelloServerFunction
+{
+    [FunctionName("HelloServerFunction")]
+    public static async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "hello")]
+        HttpRequest req, ILogger log)
+    {
+        log.LogInformation("Server received request");
+
+        var token = req.Headers["Authorization"];
+
+        return await Task.FromResult(new OkObjectResult($"Hello client, you sent this token:\n\n {token}"));
+    }
+}
